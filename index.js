d90771ccef9d9a2502ce8957bc0f8000e5fc0649
Logan Chew <logantuff@gmail.com>
2023-10-05 04:42:31 -0600

    Fixed?

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-let cats = ["cat1.jpg",
-    "cat2.jpg",
-    "cat3.jpg",
-    "cat4.jpg",
-    "cat5.jpg",
-    "cat6.jpg",
-    "cat7.jpg",
-    "cat8.jpg"
+let cats = [
+  "cat1.jpg",
+  "cat2.jpg",
+  "cat3.jpg",
+  "cat4.jpg",
+  "cat5.jpg",
+  "cat6.jpg",
+  "cat7.jpg",
+  "cat8.jpg",
 ];
-let imgCaption = ["Worker Cat",
-    "Space Cat",
-    "On the Edge Cat",
-    "Kitty Cat",
-    "Sleepy Cat",
-    "Playful Cat",
-    "Halloween Cat",
-    "Tough Cat"
+
+let imgCaption = [
+  "Worker Cat",
+  "Space Cat",
+  "On the Edge Cat",
+  "Kitty Cat",
+  "Sleepy Cat",
+  "Playful Cat",
+  "Halloween Cat",
+  "Tough Cat",
 ];
+
 let img = document.getElementById("img"),
-    caption = document.getElementById("caption");
-setInterval(function(){
-    let pickPicture = Math.floor(Math.random()*8);
-    img.src = cats[pickPicture];
-    caption.innerHTML = imgCaption[pickPicture];
-},3000);
+  caption = document.getElementById("caption");
+
+setInterval(function () {
+  let pickPicture = Math.floor(Math.random() * 8);
+
+  img.src = cats[pickPicture];
+  caption.innerHTML = imgCaption[pickPicture];
+}, 3000);
